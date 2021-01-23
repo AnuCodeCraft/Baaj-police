6c26f2ca1d0b5677d1823b97e5f6f8305642eba5
Anupriya <anupriya10031@gmail.com>
2021-01-23 12:13:44 +0530

    update

---
+++ src/Screen/Event.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Event= ()=>{
+    return(
+      <View style={{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
+      }}>
+      <Text>Event</Text>
+      </View>
+    );
+  }
+
+export default Event;  
+  
