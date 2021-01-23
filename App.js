87b338defc4689f648ffcf7d8234a61d0105a4ba
Anupriya <anupriya10031@gmail.com>
2021-01-23 12:11:06 +0530

    updated

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
+
+import  Home from "./src/Screen/Home";
+import  Event from "./src/Screen/Event";
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
   return (
-    <View style={styles.container}>
-      <Text>Good Morning</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Drawer.Navigator
+      drawerStyle={{
+        backgroundColor: '#c6cbef',
+        width: 240,
+      }}
+    >
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Event" component={Event} />
+    </Drawer.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
+  );
+}
+
+
+
